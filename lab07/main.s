1f213ffe8998d79612301eeee1f417e45aab194c
doudoujay <doudoujay@qq.com>
2017-10-28 15:53:33 -0400

    .

--- lab07/main.s
+++ lab07/main.s
@@ -20,42 +20,41 @@ scan_string: .asciz "%s" @scan string
 .balign 4
 scan_int: .asciz "%d" @scan int
 
-.balign 4 
+.balign 4
 index_1: .word 0
 
 .balign 4
 index_2: .word 0
 
-.section	.text	
+.section	.text
 .global		main
 main:
-  @Print the prompt one
-  ldr r0, =print_1
-  bl printf
-  @Read from user input, and store in input
-  ldr r0, =scan_string
-  ldr r1, =input
-  bl scanf
+@Print the prompt one
+ldr r0, =print_1
+bl printf
+@Read from user input, and store in input
+ldr r0, =scan_string
+ldr r1, =input
+bl scanf
 
-  @Print the prompt two
-    ldr r0, =print_2
-    bl printf
-    @Read from user input, and store in index_1
-    ldr r0, =scan_int
-    ldr r1, =index_1
-    bl scanf
+@Print the prompt two
+ldr r0, =print_2
+bl printf
+@Read from user input, and store in index_1
+ldr r0, =scan_int
+ldr r1, =index_1
+bl scanf
 
-    
-      @Print the prompt three
-        ldr r0, =print_3
-        bl printf
-        @Read from user input, and store in index_2
-        ldr r0, =scan_int
-        ldr r1, =index_2
-        bl scanf
-	
-	nop		@no operation
-	mov r7, $1	@exit syscall
-	svc $0		@wake kernel
-	.end
-	
+
+@Print the prompt three
+ldr r0, =print_3
+bl printf
+@Read from user input, and store in index_2
+ldr r0, =scan_int
+ldr r1, =index_2
+bl scanf
+
+nop		@no operation
+mov r7, $1	@exit syscall
+svc $0		@wake kernel
+.end
