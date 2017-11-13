d9c9dac82f6ca23a17dea870a68895f6c47ced50
Jay <doudoujay@qq.com>
2017-11-13 05:26:45 +0000

    .

--- lab08/printinteger.s
+++ lab08/printinteger.s
@@ -35,7 +35,7 @@ digit:
 	and r0, r4, r6 @ mask r6 and r4
 	lsr r0, r0, r5 @ logical shift right by amount of r5
 	ldr r2, =hex
-	add r0, r0, r2 @ r0+=r2
+	add r0, r0, r2 @ r2[r0]
 	ldrb r0, [r0]
 	bl putchar
 	lsr r6, r6, #4 @logical shift right 4 digits for mask
@@ -44,40 +44,63 @@ digit:
 	
 mov sp, fp
 pop {r4,r5,r6,fp,pc}
+
+
 	
-loop_mod:
-	@mod and save the result
-	mov r0, r4
-	b s_mod_by_16
-	mov r5, r0 
+
 	
-s_divide_by_16:
-   /* r0 contains the argument to be divided by 16 */
-   ldr r1, .Ls_magic_number_16 /* r1 ← magic_number */
+
+s_divide_by_10:
+   /* r0 contains the argument to be divided by 10 */
+   ldr r1, .Ls_magic_number_10 /* r1 ← magic_number */
    smull r1, r2, r1, r0   /* r1 ← Lower32Bits(r1*r0). r2 ← Upper32Bits(r1*r0) */
-   add r2, r2, r0         /* r2 ← r2 + r0 */
-   mov r2, r2, ASR #3     /* r2 ← r2 >> 3 */
+   mov r2, r2, ASR #2     /* r2 ← r2 >> 2 */
    mov r1, r0, LSR #31    /* r1 ← r0 >> 31 */
    add r0, r2, r1         /* r0 ← r2 + r1 */
    bx lr                  /* leave function */
    .align 4
-   .Ls_magic_number_16: .word 0x80000001
+   .Ls_magic_number_10: .word 0x66666667
+
    
-s_mod_by_16:
-	/* r0 contains the argument to be mod by 16 */
+s_mod_by_10:
+	/* r0 contains the argument to be mod by 10 */
 	mov r2, r0
-	b s_divide_by_16
-	mov r3, #16
-	mul r1, r0, r3
+	b s_divide_by_10
+	mov r3, #10
+	mul r1, r0, r3 @r1 = divide * 10
 	sub r0, r2, r1
 	bx lr
 
 
 printd:
 	/*r0: integer*/
-	push {fp,lr}
+	push {r4,r5,r6,fp,lr}
 	mov fp, sp
 
+	mov r4, r0 @r4 to hold the integer value
+	mov r6, #0 @counter
+	
+loop_mod:
+	@mod and save the result
+	mov r0, r4
+	b s_mod_by_10
+	mov r5, r0 	@use r5 to hold the mod result
+	push {r5}
+	add r6, r6, #1
+
+	@r4 = r4/10
+	mov r0, r4
+	b s_divide_by_10
+	mov r4, r0 
+	bpl loop_mod @zero or positive
+
+print_result:
+	pop {r5}
+	mov r0, r5
+	bl putchar
+	sub r6, r6, #1
+	mov r0, r6
+	bpl print_result
 	
 	mov sp, fp
 	pop {fp, pc}
